f8b268283bc1b6dafdae53a7c78e3b754e30d60f
Daniel Alonzo Herrera <dan13lalonzo@gmail.com>
2020-04-02 17:48:04 -0600

    Update Prueba30M.java

--- Prueba30M/src/prueba30m/Prueba30M.java
+++ Prueba30M/src/prueba30m/Prueba30M.java
@@ -32,6 +32,11 @@ public class Prueba30M {
         System.out.println("Nombre: Luis Humberto Ruiz Castillo");
         System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
         System.out.println("Telefono: 37070944");
+        
+        System.out.println("Daniel Alonzo Herrera ");
+        System.out.println("7a. AVE 9-81 Zona 5, Huehuetenango, Huehuetenango ");
+        System.out.println("3059-6671 ");
+        
     }
     
 }
