8ba7d621391048bbabda435d4ed69c8702e08b20
HP <HP@192.168.1.80>
2020-04-02 17:22:37 -0600

    Esta es mi prueba, atte yoisy

---
+++ Prueba30M/src/prueba30m/Prueba30M.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba30m;
+
+/**
+ *
+ * @author HP
+ */
+public class Prueba30M {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Nombre: Yoisy Yareny Lopez Herrera");
+         System.out.println("Direccion ZOna 4, Huehuetenango");
+          System.out.println("NTelefono: 45517447");
+    }
+    
+}
